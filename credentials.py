71e4e07c0f0e725bd54c57b03d4b1c22274fab64
Andres Videla <andvid72@gmail.com>
2024-08-18 11:06:30 -0300

    Imprimo id y api

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 client_id = 'YourID' #agregar el ID
-client_secret = 'YourSecret' #agregar la clave API
+client_secret = 'YourSecret' #agregar la clave
 client_url = 'wss://test.deribit.com/ws/api/v2'
